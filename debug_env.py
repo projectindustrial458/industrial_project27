d007fd4621b7ea3ae2eddfca6cf4e76fd71e00f9
Adithyan s <96611698+adithyan403@users.noreply.github.com>
2026-02-09 21:39:22 +0530

    cleand up files

--- debug_env.py
+++
@@ -1,4 +0,0 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
-print(f"MONGO_URI: {os.getenv('MONGO_URI')}")
--- test_login.bat
+++
@@ -1,1 +0,0 @@
-curl.exe -X POST http://127.0.0.1:5000/login -H "Content-Type: application/json" -d "{\"depotId\": \"TVM\", \"stationMasterId\": \"SM4272\", \"password\": \"YctpI9mW\"}"
